fe96841a23d4521334b1d043aa2c077118f84f56
paulasegati <paulasegati@hotmail.com>
2021-03-03 11:21:38 -0300

    correcao nos comentarios

--- Loteria.java
+++ Loteria.java
@@ -9,15 +9,14 @@ public class Loteria{
         ArrayList<Integer> lista = new ArrayList<>();
 		Integer valor;
         //repete até ter os 6 numeros
-        while (lista.size() < 6)
-        {
-             //imprime numero ate 60
-            // System.out.println(gerar.nextInt(61));
+        while (lista.size() < 6){ 
+            //imprime numero ate 60
             valor = gerar.nextInt(61);
             // se o numero nao estiver na lista,ele adiciona 
             if (lista.indexOf(valor) < 0 && valor!=0) {
-                lista.add(valor);    
-                System.out.println("Numero sorteado: " + valor);
+                lista.add(valor); 
+                //imprime os numeros da sorte 
+                System.out.println("Numero da sorte: " + valor);
             }
         }   
     }
